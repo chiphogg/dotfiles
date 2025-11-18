6d5835cb576190715331dd486787de678371514b
Chip Hogg <charles.r.hogg@gmail.com>
2025-11-17 20:26:01 -0500

    Tweak folds in C++

--- .config/nvim/after/ftplugin/cpp.lua
+++ .config/nvim/after/ftplugin/cpp.lua
@@ -1,3 +1,8 @@
 vim.treesitter.start()
+
+-- Use syntax-based folding from treesitter.
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+-- Folding is nice, but start with all folds open.
+vim.opt_local.foldlevel = 99
